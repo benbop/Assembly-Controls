56f00eb4cc7a5cd0fe3bd70c42b89a303b3f0963
benbop <105657554+benbop@users.noreply.github.com>
2023-12-27 18:44:59 +0200

    Update ben.asm
    Added normal fps much smoother yay

--- ben.asm
+++ ben.asm
@@ -7,9 +7,26 @@ DATASEG
 Y dw 100
 X dw 100
 Board dw 200
-DrawFlag db 0  ; Flag to control drawing
+TIME_AUX DB 0
+CheckKeyPress DB, 0
 ; --------------------------
 CODESEG
+PROC CLEAR_SCREEN              ;clear the screen by restarting the video mode
+	
+			MOV AH,00h                   ;set the configuration to video mode
+			MOV AL,13h                   ;choose the video mode
+			INT 10h    					 ;execute the configuration 
+		
+			MOV AH,0Bh 					 ;set the configuration
+			MOV BH,00h 					 ;to the background color
+			MOV BL,00h 					 ;choose black as background color
+			INT 10h    					 ;execute the configuration
+			
+			RET
+			
+	 ENDP CLEAR_SCREEN
+	
+
 proc moves
     mov ah, 01h       ; Function 01h - Check for Key Press
     int 16h
@@ -39,39 +56,37 @@ exit_moves:
     ret
 
 w_pressed:
+	
     sub [Y], 5
-	mov [DrawFlag], 1
-    jmp exit_moves
+	jmp exit_moves
+    
 
 d_pressed:
     add [X], 5
-	mov [DrawFlag], 1
-    jmp exit_moves
+	jmp exit_moves
 
 a_pressed:
     sub [X], 5
-	mov [DrawFlag], 1
     jmp exit_moves
 
 s_pressed:
     add [Y], 5
-	mov [DrawFlag], 1
     jmp exit_moves
 
 endp moves
 
-proc draw
+proc draw_player
 	mov ax, 13h
     int 10h
 	
     mov ax, 0C07h     ; Function 0Ch, Set Pixel Color
-    mov bx, 0         ; Page number (usually 0 in mode 13h)
+    mov bh, 0         ; Page number (usually 0 in mode 13h)
 	
     mov cx, [X]       ; X-coordinate
     mov dx, [Y]       ; Y-coordinate
 	mov si,0
 	mov di,0
-	mov al, 4; Color (4 is often red in mode 13h)
+	mov al, 2; Color green
 	draw_row_loop:
 	
 	draw_pixel_loop:
@@ -79,21 +94,44 @@ proc draw
 	inc di
 	
 	int 10h
-	cmp di,4
+	cmp di,16
 	jne draw_pixel_loop
-	sub cx, 4
+	sub cx, 16
 	mov di, 0
 	inc dx
 	inc si
 	
 	int 10h
-	cmp si,4
+	cmp si,2
 	jne draw_row_loop
 	
 	
     ret
-endp draw
+endp draw_player
 		
+proc draw_ground
+mov ax, 13h
+    int 10h
+	
+    mov ax, 0C07h     ; Function 0Ch, Set Pixel Color
+    mov bh, 0         ; Page number (usually 0 in mode 13h)
+	
+    mov cx, 0       ; X-coordinate
+    mov dx, 10       ; Y-coordinate
+	mov si,0
+	mov di,0
+	mov al, 15; Color white
+	
+	loopy:
+	inc  cx
+	int 10h
+	cmp cx , 320
+	jne loopy
+	
+	RET
+endp draw_ground
+mov ax, 13h
+int 10h
 
 start:
     mov ax, @data
@@ -104,20 +142,22 @@ start:
     
 
     game_loop:
-        call moves
+		CHECK_TIME:                      ;time checking loop
+			MOV AH,2Ch 					 ;get the system time
+			INT 21h 			 ;is the current time equal to the previous one(TIME_AUX)?
+			CMP DL,[TIME_AUX] 			 ;is the current time equal to the previous one(TIME_AUX)?
+			call moves
 			
-		call draw
+			JE CHECK_TIME   		     ;if it is the same, check again
+			
+;           If it reaches this point, it's because the time has passed
+  
+			MOV [TIME_AUX],DL              ;update time
+			
+			CALL CLEAR_SCREEN
+			
+		call draw_player
 		
-        ; Add a small delay to control the game speed
-        mov cx, 20000
-        delay_loop:
-            loop delay_loop
-
-    ; Check for an exit condition (e.g., ESC key press)
-    ; Add your exit condition here
-
-    ; --------------------------
-
     jmp game_loop
 
 exit:
